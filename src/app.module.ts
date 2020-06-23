c110a6d49c03188de9ffb0fbff89634fa3d9b975
Bernard <r115codes@gmail.com>
2020-06-23 09:44:10 +0300

    Initialize graphql setup.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
+import * as path from 'path';
 import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config"
 import databaseConfig from "./config/database.config";
+import { GraphQLModule } from "@nestjs/graphql"
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(databaseConfig().CONNECTION_URI)
+    MongooseModule.forRoot(databaseConfig().CONNECTION_URI),
+    GraphQLModule.forRoot({
+      context: ({ req }) => ({ req }),
+      autoSchemaFile: path.join(process.cwd(), 'schema.gql'),
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
