d518c94c20664cbc57d5c319312a1bc382efbc53
Bernard <r115codes@gmail.com>
2020-06-25 07:28:50 +0300

    Import faculty schema and services

--- src/modules/faculty/faculty.module.ts
+++ src/modules/faculty/faculty.module.ts
@@ -1,9 +1,21 @@
 import { Module } from "@nestjs/common"
+import { MongooseModule } from "@nestjs/mongoose"
+import Faculty, { FacultySchema } from "./models/faculty.schema"
+import FacultyService from "./faculty.service"
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Faculty.name,
+        schema: FacultySchema,
+      }
+    ])
+  ],
   exports: [],
-  providers: []
+  providers: [
+    FacultyService,
+  ]
 })
 
 export default class FacultyModule{};
--- src/modules/faculty/models/faculty.schema.ts
+++ src/modules/faculty/models/faculty.schema.ts
@@ -1,4 +1,4 @@
-import { Prop, Schema } from "@nestjs/mongoose"
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
 import { Document } from "mongoose"
 
 @Schema()
@@ -15,3 +15,5 @@ export default class Faculty extends Document {
   @Prop()
   title: string
 }
+
+export const FacultySchema = SchemaFactory.createForClass(Faculty);
